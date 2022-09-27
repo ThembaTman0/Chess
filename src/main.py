d281ad8d4b59e6946d98cec6ea205f9c110b08d1
ThembaTman0 <thembatman0@gmail.com>
2022-09-27 10:25:22 +0200

    V-1

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import sys
+import pygame
+# Import libraries from const
+from const import *
+
+class Main:
+    def __init__(self):
+        pass
+    def mainloop(self):
+        pass
+
+main = Main()
+main.mainloop()
