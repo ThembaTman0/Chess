a9e355916cc9d94c282474a793b8eddf5476951b
ThembaTman0 <thembatman0@gmail.com>
2022-09-28 16:07:59 +0200

    v-1.3

---
+++ src/piece.py
@@ -0,0 +1,8 @@
+class Piece:
+    def __init__(self, name, color, value, texture, texture_rec=None):
+        pass
+
+class Pawn(Piece):
+    def __init__(self,color):
+        self.dir = -1 if color== 'white' else 1
+        
