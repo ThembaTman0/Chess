d51d3c66180e4795d5a006bf3324ebd278a85094
ThembaTman0 <thembatman0@gmail.com>
2022-09-29 21:17:05 +0200

    v-1.6

--- src/piece.py
+++ src/piece.py
@@ -5,5 +5,16 @@ class Piece:
 class Pawn(Piece):
     def __init__(self,color):
         self.dir = -1 if color== 'white' else 1
-        super().__init__('pawn', color)
+        super().__init__('pawn', color, 1.0)
+
+class Knight(Piece):
+
+    def __init__(self,color):
+        super().__init__('knight', color, 3.0)
+
+
+class Bishop(Piece):
+    
+    def __init__(self,color):
+        super().__init__('bishop', color, 3.001)
         
