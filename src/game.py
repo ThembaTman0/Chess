bc09a7997de74bf63050206eede00dcac962fc35
ThembaTman0 <thembatman0@gmail.com>
2022-09-30 13:31:09 +0200

    v-1.9 chess piece on the board

--- src/game.py
+++ src/game.py
@@ -1,10 +1,13 @@
+from tkinter import S
 import pygame 
 from const import *
+from board import Board
+from piece import Piece
 
 class Game:
 
     def __init__(self):
-        pass
+        self.board =Board()
 
     #show method
 
@@ -19,3 +22,19 @@ class Game:
                 rect = (col * SQSIZE, row* SQSIZE, SQSIZE, SQSIZE)
 
                 pygame.draw.rect(surface, color, rect)
+
+    # render the pieces
+    def show_pieces(self, surface):
+        for row in range(ROWS):
+                for col in range(COLS):
+                    # is there a piece here
+
+                    if self.board.squares[row][col].has_piece():
+                        # store the piece into a varible
+                        piece = self.board.squares[row][col].piece
+                        # Load image of the piece
+                        img = pygame.image.load(piece.texture)
+                        # center the piece
+                        img_center= col * SQSIZE + SQSIZE// 2, row * SQSIZE + SQSIZE // 2
+                        piece.texture_rect= img.get_rect(center=img_center)
+                        surface.blit(img, piece.texture_rect)
--- src/piece.py
+++ src/piece.py
@@ -1,6 +1,26 @@
+import os
+
 class Piece:
     def __init__(self, name, color, value, texture= None, texture_rec=None):
-        pass
+        self.name=name
+        self.color=color
+
+        value_sign =1 if color == 'white' else -1
+        self.value=value*value_sign
+        self.moves=[]
+        self.moved=False
+        self.texture=texture
+        self.set_texture()
+        self.texture_rec=texture_rec
+
+    def set_texture(self, size = 80):
+        self.texture=os.path.join(
+            # assets/images/img-80px/black_king.png
+            f'assets/images/imgs-{size}px/{self.color}_{self.name}.png'
+        )
+    
+    def add_moves(self, move):
+        self.move.append(move)
 
 class Pawn(Piece):
     def __init__(self,color):
